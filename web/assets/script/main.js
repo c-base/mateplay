74a45c064cd36336c5be0b046ab9088b2e2f99f5
Michael Kreil <github@michael-kreil.de>
2019-02-17 22:28:10 +0100

    refactor client

--- web/assets/script/main.js
+++ web/assets/script/main.js
@@ -1,90 +1,116 @@
 $(function () {
-	var videos = [], activeVideo, inAppMode;
+	var videos = [], activeVideo, inMobileMode;
+
+	var container = $('#videos');
 
 	checkState();
 
 	$(window).resize(checkState);
 	function checkState () {
-		inAppMode = window.innerWidth < 700;
-		checkHover();
+		inMobileMode = (window.innerWidth < 700);
+		checkScrollHover();
 	}
 
-	$(window).scroll(checkHover);
+	$(window).scroll(checkScrollHover);
 
 	$('#stopButton').click(function () {
 		$.getJSON('/api/stop', function () {})
 	})
 
 	$.getJSON('/api/getvideos', function (data) {
-		videos = data;
-
-		var container = $('#videos');
 		container.html(null);
-		videos.forEach(function (video) {
-			var node = $([
-				'<div class="video">',
-					'<div class="preview" style="background-image:url(\'/assets/thumbs/'+video.thumbnailName+'\')">',
-						'<div class="playButton"></div>',
-					'</div>',
-					'<div class="title">'+video.title+'</div>',
-				'</div>'
-			].join(''));
+		videos = data.map(function (entry) { return new Video(entry) });
 
-			container.append(node);
-
-			node.on('mouseover', function () { checkHover(video) });
-			node.on('mouseout',  function () { checkHover(false) });
-
-			node.on('click', function () {
-				play(video);
-			})
-
-			video.node = node;
-			video.preview = node.find('.preview');
-			video.offset = 0;
-
-			node.find('.playButton').get(0).insertAdjacentHTML('beforeend','<svg viewBox="0 0 200 200"><circle cx="100" cy="100" r="90" fill="#000" stroke-width="15" stroke="#fff"/><polygon points="70,55 70,145 145,100" fill="#fff"/></svg>');
-		})
-		checkHover();
+		setGlobalHover(false);
 	})
 
-	function checkHover(video) {
-		if (inAppMode) {
-			var y0 = $(window).scrollTop() + window.innerHeight/2;
-			var minDistance = 1e10;
-			
-			videos.forEach(function (video) {
-				var y = video.preview.offset().top + video.preview.height()/2;
-				var distance = Math.abs(y - y0);
-				if (minDistance > distance) {
-					minDistance = distance;
-					activeVideo = video;
-				}
-			})
-		} else {
-			activeVideo = video;
-		}
-			
+	function setGlobalHover(hoverVideo) {
+		videos.forEach(function (video) { video.setHover(video === hoverVideo) });
+	}
+	
+	function checkScrollHover() {
+		if (!inMobileMode) return;
+
+		var y0 = $(window).scrollTop() + window.innerHeight/2;
+		var minDistance = 1e10;
+		var hoverVideo = false;
+		
 		videos.forEach(function (video) {
-			if (video === activeVideo) {
-				video.node.toggleClass('active', true);
-			} else {
-				video.offset = 0;
-				video.preview.css('background-position', '0 '+(-48*128-1)+'px');
-				video.node.toggleClass('active', false);
+			var y = video.getCenterY();
+			var distance = Math.abs(y - y0);
+			if (minDistance > distance) {
+				minDistance = distance;
+				hoverVideo = video;
 			}
 		})
+
+
+		setGlobalHover(hoverVideo);
 	}
 
-	setInterval(function () {
-		if (!activeVideo) return;
-		activeVideo.offset = (activeVideo.offset+1) % 96;
-		activeVideo.preview.css('background-position', '0 '+(-activeVideo.offset*128-1)+'px');
-	}, 80)
+	function Video(data) {
+		var preview, previewInterval;
+		var isHover = false;
+		var offset = 0;
 
-	function play(video) {
-		$.getJSON('/api/play/'+video.title, function (response) {
-			console.log(response);
+		var node = $([
+			'<div class="video">',
+				'<div class="preview" style="background-image:url(\'/assets/thumbs/'+data.thumbnailName+'\')">',
+					'<div class="playButton"></div>',
+				'</div>',
+				'<div class="title">'+data.title+'</div>',
+			'</div>'
+		].join(''));
+
+		preview = node.find('.preview');
+
+		container.append(node);
+
+		node.on('mouseover', function () { setGlobalHover(me) });
+		node.on('mouseout',  function () { setGlobalHover(false) });
+
+		node.on('click', function () {
+			$.getJSON('/api/play/'+data.title, function (response) {
+				console.log(response);
+			})
 		})
+
+		node.find('.playButton').get(0).insertAdjacentHTML('beforeend','<svg viewBox="0 0 200 200"><circle cx="100" cy="100" r="90" fill="#000" stroke-width="15" stroke="#fff"/><polygon points="70,55 70,145 145,100" fill="#fff"/></svg>');
+
+		var me = {
+			setHover: setHover,
+			getCenterY: function () { return preview.offset().top + preview.height()/2; }
+		}
+
+		function setHover(hover) {
+			if (hover === isHover) return;
+
+			node.toggleClass('hover', hover);
+
+			if (hover) {
+				startPreview();
+			} else {
+				stopPreview();
+			}
+
+			isHover = hover;
+		}
+
+		function startPreview() {
+			offset = 0;
+			preview.css('background-position', '0 '+(-48*128-1)+'px');
+			previewInterval = setInterval(function () {
+				offset = (offset+1) % 96;
+				preview.css('background-position', '0 '+(-offset*128-1)+'px');
+			}, 80)
+		}
+
+		function stopPreview() {
+			if (previewInterval) clearInterval(previewInterval);
+			previewInterval = false;
+
+		}
+
+		return me;
 	}
 })
